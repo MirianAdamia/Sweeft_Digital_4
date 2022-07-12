6f3b70ca9ec817d5089a6b747ad0ede68b9bb1d9
Mirian Adamia <m.adamia@Imedil.ge>
2022-07-12 18:21:21 +0400

    Add project files.

---
+++ Sweeft_Digital_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sweeft_Digital_4
+{
+ 
+    internal class Program
+    {
+        static bool IsProperly(string sequence)
+        {
+            Stack<char> stack = new Stack<char>();
+            foreach (var s in sequence)
+            {
+                if (s == '(')
+                    stack.Push(s);
+                else
+                {
+                    if (stack.Count != 0)
+                        stack.Pop();
+                    else
+                        return false;
+                }
+
+            }
+
+            if (stack.Count == 0)
+                return true;
+
+            return false;
+
+        }
+        static void Main(string[] args)
+        {
+            var result = IsProperly("()()()");
+            Console.WriteLine(result);
+        }
+    }
+    }
+
+
